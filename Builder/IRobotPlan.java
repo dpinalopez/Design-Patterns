314f621cbcf39f240f46bf2e863c85c1c4e18ef0
David Pina <ixabre@gmail.com>
2013-01-19 13:54:31 +0100

    borro el directorio Builder

--- Builder/IRobotPlan.java
+++
@@ -1,8 +0,0 @@
-package dpina.patrones.Builder;
-
-public interface IRobotPlan {
-	public void  setRobotHead(String head);
-	public void  setRobotTorso(String Torso);
-	public void  setRobotArms(String Arms);
-	public void  setRobotLegs(String legs);
-}
